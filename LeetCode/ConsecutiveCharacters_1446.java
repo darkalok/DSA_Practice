8490e7e6388a76adf61288563a5ef7cc20f5d849
Alok Jaiswal <57588918+AlokJais@users.noreply.github.com>
2021-08-29 20:38:48 +0530

    TwoSum

--- LeetCode/ConsecutiveCharacters_1446.java
+++ LeetCode/ConsecutiveCharacters_1446.java
@@ -1,16 +1,17 @@
 public class ConsecutiveCharacters_1446 {
+
     class Solution {
         public int maxPower(String s) {
             char prev = s.charAt(0);
             int count = 1;
-            int maxLength =1;
-            for(int curr=1; curr<s.length(); curr++){
+            int maxLength = 1;
+            for (int curr = 1; curr < s.length(); curr++) {
                 char cc = s.charAt(curr);
-                if(cc==prev){
+                if (cc == prev) {
                     count++;
-                }else{
-                    prev=cc;
-                    count=1;
+                } else {
+                    prev = cc;
+                    count = 1;
                 }
                 maxLength = Math.max(maxLength, count);
             }
