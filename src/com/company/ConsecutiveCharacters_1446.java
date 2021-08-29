e5da646c3a192e564a21195f6a4da08f3c9ccedc
Alok Jaiswal <57588918+AlokJais@users.noreply.github.com>
2021-08-29 20:04:14 +0530

    Initial commit

---
+++ src/com/company/ConsecutiveCharacters_1446.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class ConsecutiveCharacters_1446 {
+    class Solution {
+        public int maxPower(String s) {
+            char prev = s.charAt(0);
+            int count = 1;
+            int maxLength =1;
+            for(int curr=1; curr<s.length(); curr++){
+                char cc = s.charAt(curr);
+                if(cc==prev){
+                    count++;
+                }else{
+                    prev=cc;
+                    count=1;
+                }
+                maxLength = Math.max(maxLength, count);
+            }
+            return maxLength;
+
+        }
+    }
+}
